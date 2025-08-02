6b48d5ff7fa100afbbaa7c594d1972aac781dbcf
Sérgio Dantas <sergiodnts828@gmail.com>
2025-08-02 16:38:09 -0300

    feat(media): add django-cleanup to installed apps

--- core/settings.py
+++ core/settings.py
@@ -18,6 +18,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # django-cleanup
+    "django_cleanup",
 ]
 
 MIDDLEWARE = [
