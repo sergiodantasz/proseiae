37f5fb73704184b1d07a5fc7aebffd4915090903
Sérgio Dantas <sergiodnts828@gmail.com>
2025-08-12 01:46:43 -0300

    feat: add online users tracking and update UI for real-time chat

--- chat/consumers.py
+++ chat/consumers.py
@@ -14,12 +14,18 @@ class ChatConsumer(WebsocketConsumer):
         self.identifier = self.scope["url_route"]["kwargs"]["identifier"]
         self.chat = get_object_or_404(Chat, identifier=self.identifier)
         async_to_sync(self.channel_layer.group_add)(self.identifier, self.channel_name)
+        if self.user not in self.chat.online_users.all():
+            self.chat.online_users.add(self.user)
+            self.update_online_users_count()
         self.accept()
 
     def disconnect(self, close_code):
         async_to_sync(self.channel_layer.group_discard)(
             self.identifier, self.channel_name
         )
+        if self.user in self.chat.online_users.all():
+            self.chat.online_users.remove(self.user)
+            self.update_online_users_count()
 
     def receive(self, text_data):
         text_data_json = loads(text_data)
@@ -43,4 +49,20 @@ class ChatConsumer(WebsocketConsumer):
             "user": self.user,
         }
         html = render_to_string("chat/partials/_message-ws.html", context)
+        self.send(text_data=html)
+
+    def update_online_users_count(self):
+        online_users_count = self.chat.online_users.count()
+        event = {
+            "type": "online_users_count_handler",
+            "online_users_count": online_users_count,
+        }
+        async_to_sync(self.channel_layer.group_send)(self.identifier, event)
+
+    def online_users_count_handler(self, event):
+        online_users_count = event["online_users_count"]
+        context = {
+            "online_users_count": online_users_count,
+        }
+        html = render_to_string("chat/partials/_online-users-count.html", context)
         self.send(text_data=html)
