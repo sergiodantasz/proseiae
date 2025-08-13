9fc3de0eb4493a2724716b834b3d0203eafc0dd8
Sérgio Dantas <sergiodnts828@gmail.com>
2025-08-13 10:21:42 -0300

    refactor: convert ChatConsumer to async, update connection and disconnection handling

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,58 +1,55 @@
 from json import loads
 
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import sync_to_async
+from channels.generic.websocket import AsyncWebsocketConsumer
 from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
 
 from chat.models import Chat, Message
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.user = self.scope["user"]
         self.identifier = self.scope["url_route"]["kwargs"]["identifier"]
-        self.chat = get_object_or_404(Chat, identifier=self.identifier)
-        async_to_sync(self.channel_layer.group_add)(self.identifier, self.channel_name)
-        if self.user not in self.chat.online_users.all():
-            self.chat.online_users.add(self.user)
-            self.update_online()
-        self.accept()
+        self.chat = await self._get_object_or_404(Chat, identifier=self.identifier)
+        await self.channel_layer.group_add(self.identifier, self.channel_name)
+        online_users = await self._get_online_users()
+        if self.user not in online_users:
+            await sync_to_async(self.chat.online_users.add)(self.user)
+            await self.update_online()
+        await self.accept()
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
-            self.identifier, self.channel_name
-        )
-        if self.user in self.chat.online_users.all():
-            self.chat.online_users.remove(self.user)
-            self.update_online()
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.identifier, self.channel_name)
+        online_users = await self._get_online_users()
+        if self.user in online_users:
+            await sync_to_async(self.chat.online_users.remove)(self.user)
+            await self.update_online()
 
-    def receive(self, text_data):
-        text_data_json = loads(text_data)
-        content = text_data_json.get("content", "")
-        message = Message.objects.create(
-            chat=self.chat,
-            sender=self.user,
-            content=content,
+    async def receive(self, text_data):
+        data = loads(text_data)
+        content = data.get("content", "")
+        message = await sync_to_async(Message.objects.create)(
+            chat=self.chat, sender=self.user, content=content
         )
         event = {
             "type": "message_handler",
             "message_id": message.id,
         }
-        async_to_sync(self.channel_layer.group_send)(self.identifier, event)
+        await self.channel_layer.group_send(self.identifier, event)
 
-    def message_handler(self, event):
-        message_id = event["message_id"]
-        message = get_object_or_404(Message, id=message_id)
+    async def message_handler(self, event):
+        message = await self._get_object_or_404(Message, id=event["message_id"])
         context = {
             "message": message,
             "user": self.user,
         }
-        html = render_to_string("chat/partials/_message-ws.html", context)
-        self.send(text_data=html)
+        html = await self._render("chat/partials/_message-ws.html", context)
+        await self.send(text_data=html)
 
-    def update_online(self):
-        online_users_count = self.chat.online_users.count()
+    async def update_online(self):
+        online_users_count = await sync_to_async(self.chat.online_users.count)()
         if self.chat.chat_type == "private":
             event = {
                 "type": "online_user_status_handler",
@@ -63,20 +60,27 @@ class ChatConsumer(WebsocketConsumer):
                 "type": "online_users_count_handler",
                 "online_users_count": online_users_count,
             }
-        async_to_sync(self.channel_layer.group_send)(self.identifier, event)
+        await self.channel_layer.group_send(self.identifier, event)
 
-    def online_users_count_handler(self, event):
-        online_users_count = event["online_users_count"]
+    async def online_users_count_handler(self, event):
         context = {
-            "online_users_count": online_users_count,
+            "online_users_count": event["online_users_count"],
         }
-        html = render_to_string("chat/partials/_online-users-count.html", context)
-        self.send(text_data=html)
+        html = await self._render("chat/partials/_online-users-count.html", context)
+        await self.send(text_data=html)
 
-    def online_user_status_handler(self, event):
-        is_user_online = event["is_user_online"]
+    async def online_user_status_handler(self, event):
         context = {
-            "is_user_online": is_user_online,
+            "is_user_online": event["is_user_online"],
         }
-        html = render_to_string("chat/partials/_online-user-status.html", context)
-        self.send(text_data=html)
+        html = await self._render("chat/partials/_online-user-status.html", context)
+        await self.send(text_data=html)
+
+    async def _render(self, template_name, context):
+        return await sync_to_async(render_to_string)(template_name, context)
+
+    async def _get_online_users(self):
+        return await sync_to_async(list)(self.chat.online_users.all())
+
+    async def _get_object_or_404(self, model, *args, **kwargs):
+        return await sync_to_async(get_object_or_404)(model, *args, **kwargs)
